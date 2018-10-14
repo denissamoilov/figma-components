6adb0521efd66cc7ea1b657da93ad3d59cd808b3
Deniss Samoilov <denis@hebsdigital.com>
2018-10-14 21:09:11 +0300

    Added gatsby js

---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/denisssamoilov/Sites/figma-components/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("/Users/denisssamoilov/Sites/figma-components/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+exports.data = () => import("/Users/denisssamoilov/Sites/figma-components/.cache/data.json")
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/denisssamoilov/Sites/figma-components/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/denisssamoilov/Sites/figma-components/src/pages/index.js"))
+}
+
